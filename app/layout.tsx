5aa6a32026d2444f6dd8c296a86a0d8a22fe9cea
Juan Lucas Umali <85742113+juanlucasumali@users.noreply.github.com>
2024-11-29 16:21:42 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Consider This",
+  title: "Hypercube Test",
   description: "Challenge what you know (and what you don't)!",
 };
 
